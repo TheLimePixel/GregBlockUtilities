a4c34be82c305f6c73f482dfb98dcdd8aac11ada
EmosewaPixel <awesomeggaming@gmail.com>
2018-08-13 17:15:57 +0200

    Initial commit

---
+++ src/main/java/gregblockutils/CommonProxy.java
@@ -0,0 +1,47 @@
+package gregblockutils;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.registries.IForgeRegistry;
+
+import java.util.function.Function;
+
+@Mod.EventBusSubscriber(modid = GregBlockUtils.MODID)
+public class CommonProxy {
+
+    public void preInit()
+    {
+
+    }
+    public void postInit()
+    {
+
+    }
+
+
+    @SubscribeEvent
+    public static void registerBlocks(RegistryEvent.Register<Block> event) {
+        IForgeRegistry<Block> registry = event.getRegistry();
+    }
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event) {
+        IForgeRegistry<Item> registry = event.getRegistry();
+    }
+
+    private static <T extends Block> ItemBlock createItemBlock(T block, Function<T, ItemBlock> producer) {
+        ItemBlock itemBlock = producer.apply(block);
+        itemBlock.setRegistryName(block.getRegistryName());
+        return itemBlock;
+    }
+
+    @SubscribeEvent
+    public static void registerRecipes(RegistryEvent.Register<IRecipe> event) {
+    }
+
+
+}
---
+++ src/main/java/gregblockutils/GregBlockUtils.java
@@ -0,0 +1,45 @@
+package gregblockutils;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = GregBlockUtils.MODID,
+        name = GregBlockUtils.NAME,
+        version = GregBlockUtils.VERSION,
+        dependencies = "required-after:GregicAdditions"
+)
+public class GregBlockUtils {
+    public static final String MODID = "gregblockutils";
+    public static final String NAME = "GregBlock Utilities";
+    public static final String VERSION = "@VERSION@";
+
+    @SidedProxy(
+            modId = MODID,
+            clientSide = "gregblockutils.ClientProxy",
+            serverSide = "gregblockutils.CommonProxy"
+    )
+    public static CommonProxy proxy;
+
+    private static Logger logger;
+
+    public GregBlockUtils() {
+    }
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit();
+    }
+}
