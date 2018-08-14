feebc9bb2596b6d05c0d41baeedb8eeba5fc24e6
Bartz24 <bartz24games@gmail.com>
2018-08-13 20:58:33 -0400

    added sieve recipes to electric sieve

--- src/main/java/gregblockutils/Recipes/GBRecipeAddition.java
+++ src/main/java/gregblockutils/Recipes/GBRecipeAddition.java
@@ -1,6 +1,23 @@
 package gregblockutils.Recipes;
 
+import exnihilocreatio.compatibility.jei.sieve.SieveRecipe;
+import exnihilocreatio.registries.manager.ExNihiloRegistryManager;
+import exnihilocreatio.registries.types.Siftable;
+import gregtech.api.recipes.Recipe;
+import gregtech.api.recipes.builders.SimpleRecipeBuilder;
+import net.minecraft.item.ItemStack;
+
 public class GBRecipeAddition {
     public static void postInit() {
+        for (SieveRecipe recipe : ExNihiloRegistryManager.SIEVE_REGISTRY.getRecipeList()) {
+
+            SimpleRecipeBuilder builder = GBRecipeMaps.SIEVE_RECIPES.recipeBuilder();
+            builder.notConsumable(recipe.getMesh()).inputs((ItemStack) recipe.getInputs().get(0));
+            for (Siftable siftable : ExNihiloRegistryManager.SIEVE_REGISTRY.getDrops((ItemStack) recipe.getInputs().get(0))) {
+                builder.chancedOutput(siftable.getDrop().getItemStack(), (int) (siftable.getChance() * (float) Recipe.getMaxChancedValue()));
+            }
+            builder.duration(20).EUt(16);
+            builder.buildAndRegister();
+        }
     }
 }
