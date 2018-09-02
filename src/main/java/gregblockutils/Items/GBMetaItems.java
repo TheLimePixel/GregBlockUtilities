67844db4c31f1e63918eeb362da3b76e3f486cc0
EmosewaPixel <awesomeggaming@gmail.com>
2018-09-02 19:42:31 +0200

    That would have caused a huge problem

--- src/main/java/gregblockutils/Items/GBMetaItems.java
+++ src/main/java/gregblockutils/Items/GBMetaItems.java
@@ -5,7 +5,7 @@ public class GBMetaItems {
     public static void preInit() {
         ChunkMetaItem chunk = new ChunkMetaItem();
         GBMetaItem item = new GBMetaItem();
-        chunk.setRegistryName("chunk_meta_item");
-        item.setRegistryName("gb_meta_item");
+        chunk.setRegistryName("gb_meta_item");
+        item.setRegistryName("gb_meta_item1");
     }
 }
